623ed8541c5e077c7592727fc14f4e443a305cd3
Piotr20 <piotrpospiech00@gmail.com>
2019-09-25 12:47:24 +0200

    bar

--- scripts.js
+++ scripts.js
@@ -9,3 +9,14 @@ img1.addEventListener('click', function () {
      img1.setAttribute('id', 'big');
      img2.removeAttribute('id', 'big');
 })
+
+const i = document.querySelector('nav>i');
+const inp = document.querySelector('nav>input')
+
+i.addEventListener('mouseover', function () {
+     inp.style.width = '17vw';
+})
+
+inp.addEventListener('blur', function () {
+     inp.style.width = '0vw';
+})
